136c73797f7ab9b44bae4f36981936eef3d84fe7
bGillooley <gillooley@gmail.com>
2024-05-27 23:28:49 +0100

    Added Weather, Trains, Tides components

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["maps.googleapis.com", "cdn.weatherapi.com"],
+  },
+};
 
 export default nextConfig;
