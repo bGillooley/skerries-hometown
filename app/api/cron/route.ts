276626375982221f71e73119e6850fb6c034279c
bGillooley <gillooley@gmail.com>
2024-06-23 11:43:35 +0100

    trying to get google fonts to load on a stupid iphone

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,6 +1,8 @@
 import { revalidatePath, revalidateTag } from "next/cache";
 import type { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export function GET(request: NextRequest) {
   const authHeader = request.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
